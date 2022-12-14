09d710798356a7102ed4eb18ec7d9fb57f4554d5
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-13 20:12:27 -0600

    Creating Loader

---
+++ src/Components/LoaderComponent/LoaderComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const LoaderComponent = () => {
+    return (
+        <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default LoaderComponent
